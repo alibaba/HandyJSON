cf8af5ed79024e5bd46a00246ca4031bad636b81
周卓 <yecan.xyc@alibaba-inc.com>
2016-09-27 17:37:40 +0800

    add mutating modifier for struct

--- HandyJSON/Property.swift
+++ HandyJSON/Property.swift
@@ -57,11 +57,11 @@ extension Property {
 
 public protocol HandyJSON: Property {
     init()
-    func mapping(mapper: Mapper)
+    mutating func mapping(mapper: Mapper)
 }
 
 public extension HandyJSON {
-    func mapping(mapper: Mapper) {}
+    public mutating func mapping(mapper: Mapper) {}
 }
 
 protocol BasePropertyProtocol: HandyJSON {
