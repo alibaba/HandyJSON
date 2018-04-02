c61f19d1e948a1c1205da9b826b4ecf6a62e5360
Cocowyr <15940926841@163.com>
2018-04-02 15:11:45 +0800

    update to swift 4.1

--- Source/NominalType.swift
+++ Source/NominalType.swift
@@ -31,7 +31,7 @@ extension NominalType {
             // swift class created dynamically in objc-runtime didn't have valid nominalTypeDescriptor
             return nil
         }
-        return NominalTypeDescriptor(pointer: relativePointer(base: base, offset: base.pointee))
+        return NominalTypeDescriptor(pointer: relativePointer(base: base, offset: base.pointee - base.hashValue))
     }
 
     var fieldTypes: [Any.Type]? {
