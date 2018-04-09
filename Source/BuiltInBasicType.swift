94733e2183d3f370eaae5757f59beeb26e04ac0d
周卓 <yecan.xyc@alibaba-inc.com>
2018-04-09 14:31:12 +0800

    (dev) 16 maximumFractionDigits is enough

--- Source/BuiltInBasicType.swift
+++ Source/BuiltInBasicType.swift
@@ -117,7 +117,7 @@ fileprivate let formatter: NumberFormatter = {
     let formatter = NumberFormatter()
     formatter.usesGroupingSeparator = false
     formatter.numberStyle = .decimal
-    formatter.maximumFractionDigits = 100
+    formatter.maximumFractionDigits = 16
     return formatter
 }()
 
