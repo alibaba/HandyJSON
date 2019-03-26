de125516ef00f13310b063b2f350f80c4594d2b4
chantu <chantu.lhl@alibaba-inc.com>
2019-03-26 17:22:32 +0800

    silence warnings

--- Source/Deserializer.swift
+++ Source/Deserializer.swift
@@ -23,19 +23,19 @@ public extension HandyJSON {
 
     /// Finds the internal dictionary in `dict` as the `designatedPath` specified, and converts it to a Model
     /// `designatedPath` is a string like `result.data.orderInfo`, which each element split by `.` represents key of each layer
-    public static func deserialize(from dict: NSDictionary?, designatedPath: String? = nil) -> Self? {
+    static func deserialize(from dict: NSDictionary?, designatedPath: String? = nil) -> Self? {
         return deserialize(from: dict as? [String: Any], designatedPath: designatedPath)
     }
 
     /// Finds the internal dictionary in `dict` as the `designatedPath` specified, and converts it to a Model
     /// `designatedPath` is a string like `result.data.orderInfo`, which each element split by `.` represents key of each layer
-    public static func deserialize(from dict: [String: Any]?, designatedPath: String? = nil) -> Self? {
+    static func deserialize(from dict: [String: Any]?, designatedPath: String? = nil) -> Self? {
         return JSONDeserializer<Self>.deserializeFrom(dict: dict, designatedPath: designatedPath)
     }
 
     /// Finds the internal JSON field in `json` as the `designatedPath` specified, and converts it to a Model
     /// `designatedPath` is a string like `result.data.orderInfo`, which each element split by `.` represents key of each layer
-    public static func deserialize(from json: String?, designatedPath: String? = nil) -> Self? {
+    static func deserialize(from json: String?, designatedPath: String? = nil) -> Self? {
         return JSONDeserializer<Self>.deserializeFrom(json: json, designatedPath: designatedPath)
     }
 }
@@ -44,17 +44,17 @@ public extension Array where Element: HandyJSON {
 
     /// if the JSON field finded by `designatedPath` in `json` is representing a array, such as `[{...}, {...}, {...}]`,
     /// this method converts it to a Models array
-    public static func deserialize(from json: String?, designatedPath: String? = nil) -> [Element?]? {
+    static func deserialize(from json: String?, designatedPath: String? = nil) -> [Element?]? {
         return JSONDeserializer<Element>.deserializeModelArrayFrom(json: json, designatedPath: designatedPath)
     }
 
     /// deserialize model array from NSArray
-    public static func deserialize(from array: NSArray?) -> [Element?]? {
+    static func deserialize(from array: NSArray?) -> [Element?]? {
         return JSONDeserializer<Element>.deserializeModelArrayFrom(array: array)
     }
 
     /// deserialize model array from array
-    public static func deserialize(from array: [Any]?) -> [Element?]? {
+    static func deserialize(from array: [Any]?) -> [Element?]? {
         return JSONDeserializer<Element>.deserializeModelArrayFrom(array: array)
     }
 }
